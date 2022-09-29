34a4c520e2c42375bf554cf7dca326fba37e581d
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-09-29 19:17:56 -0400

    get google maps api key

---
+++ src/createScriptTag.ts
@@ -0,0 +1,7 @@
+export const createScriptTag = () => {
+    const body = document.body as HTMLBodyElement;
+    const indexScript = document.getElementsByTagName('script')[0];
+    const mapScript = document.createElement('script');
+    mapScript.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.GOOGLE_MAPS_API_KEY}`;
+    body.insertBefore(mapScript, indexScript);
+};
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import { User } from './User';
 import { Company } from './Company';
+import { createScriptTag } from './createScriptTag';
 
 const user = new User();
 console.log(user);
 
 const company = new Company();
 console.log(company);
+
+createScriptTag();
